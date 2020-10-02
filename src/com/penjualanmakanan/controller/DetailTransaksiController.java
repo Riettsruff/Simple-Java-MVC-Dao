353309ca3506ed73ea27e6762bfec14a4df79b46
Rietts Andreas Ruff <Riettsruff@gmail.com>
2020-10-02 18:31:49 +0700

    Add getTotalHargaByIdTransaksi & getTotalBarangByIdTransaksi Method On DetailTransaksiController

--- src/com/penjualanmakanan/controller/DetailTransaksiController.java
+++ src/com/penjualanmakanan/controller/DetailTransaksiController.java
@@ -23,29 +23,73 @@ public class DetailTransaksiController {
         conn = (Connection) new Koneksi().getKoneksi();
     }
     
-     public List<DetailTransaksi> getAllDetailTransaksi() {
-        List<DetailTransaksi> listDetailTransaksi = new ArrayList<>();
-        
-        try {
-            PreparedStatement ps = conn.prepareStatement("SELECT id, jml_barang, id_barang, id_transaksi FROM `detail_transaksi` ");
-            ResultSet rs = ps.executeQuery();
-            
-            while(rs.next()) {
-                DetailTransaksi detailtransaksi = new DetailTransaksi();
-                
-                detailtransaksi.setId(rs.getInt("id"));
-                detailtransaksi.setjml_Barang(rs.getInt("jml_barang"));
-                detailtransaksi.setidBarang(rs.getString("id_barang"));
-                detailtransaksi.setidTransaksi(rs.getString("id_transaksi"));
-                                               
-                listDetailTransaksi.add(detailtransaksi);
-            }
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        return listDetailTransaksi;
-    }
+    public List<DetailTransaksi> getAllDetailTransaksi() {
+       List<DetailTransaksi> listDetailTransaksi = new ArrayList<>();
+
+       try {
+           PreparedStatement ps = conn.prepareStatement("SELECT id, jml_barang, id_barang, id_transaksi FROM `detail_transaksi` ");
+           ResultSet rs = ps.executeQuery();
+
+           while(rs.next()) {
+               DetailTransaksi detailtransaksi = new DetailTransaksi();
+
+               detailtransaksi.setId(rs.getInt("id"));
+               detailtransaksi.setjml_Barang(rs.getInt("jml_barang"));
+               detailtransaksi.setidBarang(rs.getString("id_barang"));
+               detailtransaksi.setidTransaksi(rs.getString("id_transaksi"));
+
+               listDetailTransaksi.add(detailtransaksi);
+           }
+       } catch (SQLException se) {
+           se.printStackTrace();
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+
+       return listDetailTransaksi;
+   }
+    
+   public long getTotalHargaByIdTransaksi(String idTransaksi) {
+       long totalHarga = 0;
+       
+       try {
+           String query = "SELECT SUM(b.jml_barang * c.harga) AS \"Total Harga\" FROM transaksi a INNER JOIN detail_transaksi b ON a.id = b.id_transaksi LEFT JOIN barang c ON b.id_barang = c.id";
+           query += " WHERE a.id = \"" + idTransaksi + "\"";
+           
+           PreparedStatement ps = conn.prepareStatement(query);
+           ResultSet rs = ps.executeQuery();
+           
+           if (rs.next()) {
+               totalHarga = rs.getInt(1);
+           } 
+       } catch (SQLException se) {
+           se.printStackTrace();
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       
+       return totalHarga;
+   }
+   
+   public int getTotalBarangByIdTransaksi(String idTransaksi) {
+       int totalBarang = 0;
+       
+       try {
+           String query = "SELECT SUM(b.jml_barang) AS \"total_barang\" FROM transaksi a INNER JOIN detail_transaksi b ON a.id = b.id_transaksi";
+           query += " WHERE a.id = \"" + idTransaksi + "\"";
+           
+           PreparedStatement ps = conn.prepareStatement(query);
+           ResultSet rs = ps.executeQuery();
+           
+           if (rs.next()) {
+               totalBarang = rs.getInt(1);
+           } 
+       } catch (SQLException se) {
+           se.printStackTrace();
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       
+       return totalBarang;
+   }
 }
