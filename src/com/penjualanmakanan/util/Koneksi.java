62db5d87fd13a32337fb0399d00c8c0717e79337
Rietts Andreas Ruff <Riettsruff@gmail.com>
2020-10-02 08:19:21 +0700

    Update Koneksi Util

--- src/com/penjualanmakanan/util/Koneksi.java
+++ src/com/penjualanmakanan/util/Koneksi.java
@@ -5,10 +5,38 @@
  */
 package com.penjualanmakanan.util;
 
-/**
- *
- * @author Riett
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Koneksi {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static final String DB_URL = "jdbc:mysql://localhost/penjualan_makanan";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "";
     
+    static Koneksi conn;
+    
+    public Koneksi() {
+        try {
+            // Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+            
+            // Open a connection
+            conn = (Koneksi) DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch(SQLException se){
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }
+    }
+
+    public static Koneksi getKoneksi() {
+        return conn;
+    }
 }
