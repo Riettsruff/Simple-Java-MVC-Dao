5bbcf2cdfc2a8440fc16adac8ecadc0f2908d079
adnanghofarm <adnangofar.ag@gmail.com>
2020-10-04 23:29:13 +0700

    resolve error

---
+++ src/com/penjualanmakanan/controller/BarangController.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.penjualanmakanan.controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.penjualanmakanan.util.Koneksi;
+import com.penjualanmakanan.model.Barang;
+import static com.penjualanmakanan.util.Koneksi.Koneksi;
+
+/**
+ *
+ * @author Riett
+ */
+public class BarangController {
+
+    Connection conn;
+
+    public BarangController() {
+        conn = Koneksi();
+    }
+
+    public List<Barang> getAllBarang() {
+        List<Barang> listBarang = new ArrayList<>();
+        
+        try {
+            String query = "SELECT id, nama, stok, harga FROM `barang`";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Barang barang = new Barang();
+
+                barang.setId(rs.getString("id"));
+                barang.setNama(rs.getString("nama"));
+                barang.setStok(rs.getInt("stok"));
+                barang.setHarga(rs.getInt("harga"));
+
+                listBarang.add(barang);
+            }
+
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return listBarang;
+
+    }
+
+    public boolean deleteBarang(Barang barang) {
+        try {
+            String query = "DELETE From barang WHERE id=?";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, barang.getId());
+
+            if (ps.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return false;
+    }
+
+    public boolean insertBarang(Barang barang) {
+        try {
+            String query = "INSERT INTO barang (id, nama, stok, harga) VALUES (?, ?, ?, ?)";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, barang.getId());
+            ps.setString(2, barang.getNama());
+            ps.setInt(3, barang.getStok());
+            ps.setInt(4, barang.getHarga());
+
+            if (ps.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public int getStokByIdBarang(String idBarang) {
+        int stok = 0;
+
+        try {
+            String query = "SELECT stok FROM barang WHERE id = ?";
+
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, idBarang);
+                    
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                stok = rs.getInt(1);
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return stok;
+    }
+
+    public boolean updateBarang(Barang barang) {
+        try {
+            String query = "UPDATE barang SET nama = ?, stok = ?, harga = ? WHERE id = ?";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, barang.getNama());
+            ps.setInt(2, barang.getStok());
+            ps.setInt(3, barang.getHarga());
+            ps.setString(4, barang.getId());
+            
+            if (ps.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
---
+++ src/com/penjualanmakanan/util/Koneksi.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.penjualanmakanan.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+public class Koneksi {
+    // JDBC driver name and database URL
+    //  Database credentials
+    private final String USER = "root";
+    private final String PASS = "";
+    private static final String DB_URL = "jdbc:mysql://localhost/penjualan_makanan";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "";
+    
+    private static Connection conn;
+    
+    public static Connection Koneksi() {
+        if(conn == null) {
+            try {
+                MysqlDataSource dataSource = new MysqlDataSource();
+
+                dataSource.setUser(DB_USER);
+                dataSource.setPassword(DB_PASS);
+                dataSource.setUrl(DB_URL);
+                
+                conn = dataSource.getConnection();
+            } catch(SQLException se){
+                // Handle errors for JDBC
+                se.printStackTrace();
+            } catch(Exception e){
+                //Handle errors for Class.forName
+                e.printStackTrace();
+            }
+        }
+        
+        return conn;
+    }
+}
