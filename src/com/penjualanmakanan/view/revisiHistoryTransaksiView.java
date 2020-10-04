7c396a173788f87b0e64df562f82257164411222
mari4rossa <mari4rossa@gmail.com>
2020-10-04 22:32:55 +0700

    revisi view

--- src/com/penjualanmakanan/view/revisiHistoryTransaksiView.java
+++ src/com/penjualanmakanan/view/revisiHistoryTransaksiView.java
@@ -30,8 +30,8 @@ public class revisiHistoryTransaksiView extends javax.swing.JFrame {
         jInternalFrame1 = new javax.swing.JInternalFrame();
         jInternalFrame2 = new javax.swing.JInternalFrame();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        Button_Back4 = new javax.swing.JButton();
+        Tabel_Riwayat = new javax.swing.JTable();
+        Button_Back = new javax.swing.JButton();
 
         jInternalFrame1.setVisible(true);
 
@@ -50,7 +50,7 @@ public class revisiHistoryTransaksiView extends javax.swing.JFrame {
 
         jInternalFrame2.setVisible(true);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        Tabel_Riwayat.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -71,12 +71,12 @@ public class revisiHistoryTransaksiView extends javax.swing.JFrame {
                 "Id", "Tanggal", "Waktu", "Total"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(Tabel_Riwayat);
 
-        Button_Back4.setText("Back");
-        Button_Back4.addActionListener(new java.awt.event.ActionListener() {
+        Button_Back.setText("Back");
+        Button_Back.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Button_Back4Button_BackActionPerformed(evt);
+                Button_BackButton_BackActionPerformed(evt);
             }
         });
 
@@ -89,7 +89,7 @@ public class revisiHistoryTransaksiView extends javax.swing.JFrame {
                 .addGroup(jInternalFrame2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 584, Short.MAX_VALUE)
                     .addGroup(jInternalFrame2Layout.createSequentialGroup()
-                        .addComponent(Button_Back4, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(Button_Back, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -99,7 +99,7 @@ public class revisiHistoryTransaksiView extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(Button_Back4)
+                .addComponent(Button_Back)
                 .addContainerGap())
         );
 
@@ -123,9 +123,9 @@ public class revisiHistoryTransaksiView extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void Button_Back4Button_BackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Button_Back4Button_BackActionPerformed
+    private void Button_BackButton_BackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Button_BackButton_BackActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_Button_Back4Button_BackActionPerformed
+    }//GEN-LAST:event_Button_BackButton_BackActionPerformed
 
     /**
      * @param args the command line arguments
@@ -163,10 +163,10 @@ public class revisiHistoryTransaksiView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Button_Back4;
+    private javax.swing.JButton Button_Back;
+    private javax.swing.JTable Tabel_Riwayat;
     private javax.swing.JInternalFrame jInternalFrame1;
     private javax.swing.JInternalFrame jInternalFrame2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
